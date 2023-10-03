f5abbaaee215bcf5ae6489ca4cca86425479caab
aidan <89723084+aidan-ngiau@users.noreply.github.com>
2023-10-03 18:53:05 +0800

    Update homework_week10.js

--- homework_week10.js
+++ homework_week10.js
@@ -1,9 +1,10 @@
 var vg_1 = "week10homework.vg.json";
+var vg_2 = "week9.vg.json";
+
 vegaEmbed("#homework_week10_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "week9.vg.json";
 vegaEmbed("#homework_week9_chart", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
