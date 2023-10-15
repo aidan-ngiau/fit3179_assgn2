23306b0a51741041c0de6937d34b530bd4096ee9
aidan <aidan.ngiau@gmail.com>
2023-10-16 01:09:31 +0800

    embedding line chart in webpage

--- homework_week10.js
+++ homework_week10.js
@@ -1,6 +1,7 @@
 var vg_1 = "week10homework.vg.json";
 var vg_2 = "week9.vg.json";
 var vg_3 = "isotope.vg.json";
+var vg_4 = "line.vg.json";
 
 vegaEmbed("#homework_week10_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -11,6 +12,10 @@ vegaEmbed("#homework_week9_chart", vg_2).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#isotope_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#line_chart", vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
