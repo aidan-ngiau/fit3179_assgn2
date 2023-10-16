08197966b0f9dcf9e10c7351b4958fcc5903f7cc
aidan <aidan.ngiau@gmail.com>
2023-10-17 02:32:06 +0800

    added better line chart to dashboard

--- homework_week10.js
+++ homework_week10.js
@@ -1,7 +1,7 @@
 var vg_1 = "week10homework.vg.json";
 var vg_2 = "week9.vg.json";
 var vg_3 = "isotope.vg.json";
-var vg_4 = "line.vg.json";
+var vg_4 = "line2.vg.json";
 
 vegaEmbed("#homework_week10_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
